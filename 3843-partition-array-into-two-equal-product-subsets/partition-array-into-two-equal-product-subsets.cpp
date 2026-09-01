49bcdac8cb31b3ac3904b5c114bdd470bacaa1dc
Anirban Maity <157023094+Ani2005maity@users.noreply.github.com>
2026-09-01 23:38:00 +0530

    Time: 7 ms (32.53%) | Memory: 23.8 MB (72.59%) - LeetSync

---
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkEqualPartitions(vector<int>& nums, long long target) {
+        int n = nums.size();
+        for (int mask = 1; mask < (1 << n) - 1; mask++) {
+            long long product1 = 1;
+            long long product2 = 1;
+
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    product1 *= nums[i];
+                } else {
+                    product2 *= nums[i];
+                }
+                if (product1 > target || product2 > target)
+                    break;
+            }
+
+            if (product1 == target && product2 == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
